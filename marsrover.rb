c1177bdbff9c61d546bf0c3405722dca399d3210
Philippe Schwyter <pschwyter90@gmail.com>
2014-10-23 14:43:44 -0400

    made rover_mission method that asks only for instructions input

--- marsrover.rb
+++ marsrover.rb
@@ -68,50 +68,41 @@ end
 
 
 
-# def rover_mission
+def rover_mission
 
-# 	# puts "Enter the plateau's coordinates separated by spaces (ie. 3 3): "
-# 	# plateau_coordinates = gets.chomp
-# 	# plateau_coordinates_arr = plateau_coordinates.split.map { |element| element.to_i }
-# 	# print plateau_coordinates_arr
+	# puts "Enter the plateau's coordinates separated by spaces (ie. 3 3): "
+	# plateau_coordinates = gets.chomp
+	# plateau_coordinates_arr = plateau_coordinates.split.map { |element| element.to_i }
+	# print plateau_coordinates_arr
 
-# 	# puts "Enter the rover's initial position and heading"
-# 	# rover_initial_state = gets.chomp
-# 	# rover_initial_state_arr = rover_initial_state.split
-# 	# rover_initial_coords_arr = 
-# 	# print rover_initial_state_arr
+	# puts "Enter the rover's initial position and heading"
+	# rover_initial_state = gets.chomp
+	# rover_initial_state_arr = rover_initial_state.split
+	# rover_initial_coords_arr = 
+	# print rover_initial_state_arr
 
-# 	puts "Enter a series of instructions for the rover to execute (ie. L, R or M): "
-# 	rover_instructions = gets.chomp
-# 	rover_instructions_arr = rover_instructions.upcase.split("")
-# 	print rover_instructions_arr
+	puts "Enter a series of instructions for the rover to execute (ie. L, R or M): "
+	rover_instructions = gets.chomp
+	rover_instructions_arr = rover_instructions.upcase.split("")
+	print rover_instructions_arr
 
-# 	mars_sea = Plateau.new(5,5) # define grid size
+	mars_sea = Plateau.new(5,5) # define grid size
 
-# 	curiosity = Rover.new(0,0,"N") # create initial rover position and heading
+	curiosity = Rover.new(0,0,"N") # create initial rover position and heading
 
-# 	number_instructions = rover_instructions_arr.length
+	number_instructions = rover_instructions_arr.length
 
-# 	index = 0
-# 	number_instructions.times do 
-# 		puts rover_instructions_arr[index]
-# 		puts index
-# 		curiosity.read_instruction(rover_instructions_arr[index])
-# 		curiosity.move
-# 		curiosity.turn
-# 		index += 1
-# 	end
-# 	puts curiosity
-# end
+	index = 0
+	number_instructions.times do 
+		curiosity.read_instruction(rover_instructions_arr[index])
+		curiosity.move
+		curiosity.turn
+		index += 1
+	end
+	puts curiosity
+end
 
-# rover_mission
-
-curiosity = Rover.new(0,0,"N")
-puts curiosity
-curiosity.read_instruction("M")
-curiosity.move
-curiosity.move
-puts curiosity
+rover_mission
 
 
 
