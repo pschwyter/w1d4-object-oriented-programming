b25c51e5b1a72f101da97e84e095989ce0f0ae08
Philippe Schwyter <pschwyter90@gmail.com>
2014-10-23 13:09:05 -0400

    added step 6 to people.rb

--- people.rb
+++ people.rb
@@ -1,24 +1,27 @@
-class Student < People
+class Person
+	attr_accessor :name
+
+	def initialize(name) #Step 4
+		@name = name
+	end
+
+	def greet #Step 5
+		puts "Hi, my name is #{@name}"
+	end
+end
+
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor < People
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
 
-class Person
-	attr_accessor :name
-
-	def initialize(name)
-		@name = name
-	end
-
-	def greet
-		puts "Hi, my name is #{@name}"
-	end
-end
-
+#Step 6, create instructor named Chris and call greet
+chris = Instructor.new("Chris")
+chris.greet
