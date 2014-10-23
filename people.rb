e5adcca82f22968752773d5f6a7d450ef461d629
Philippe Schwyter <pschwyter90@gmail.com>
2014-10-23 13:05:14 -0400

    added step 4 to people.rb

--- people.rb
+++ people.rb
@@ -1,12 +1,20 @@
-class Student
+class Student < People
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < People
 	def teach
 		puts "Everything in Ruby is an Object"
+	end
+end
+
+class Person
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
 	end
 end
 
